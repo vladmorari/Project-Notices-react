da4c8a90d13576f729299a28493bc3b01a0e8bef
vladmorari <morariv67@gmail.com>
2021-03-29 21:18:58 +0300

    *

--- src/pages/Notice/Notice.tsx
+++ src/pages/Notice/Notice.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams,useHistory } from "react-router-dom";
 import * as c from "../../components";
 import * as s from "./style";
 interface Params {
   id: string;
 }
 export const Notice: React.FC = () => {
+  const history = useHistory();
   const [noticeData, setNoticeData] = React.useState({
     title: "",
     content: "",
   });
 
+  const goToNotice=()=>{
+    history.push("/notices")
+  }
   const params: Params = useParams();
   useEffect(() => {
     const token: any = localStorage.getItem("token");
@@ -34,7 +38,7 @@ export const Notice: React.FC = () => {
     <>
       <c.Header />
       <s.Notice>
-        <s.Close>X</s.Close>
+        <s.Close onClick={goToNotice}>X</s.Close>
         <h3> {`${noticeData.title}`}</h3>
         <h3> {`${noticeData.content}`}</h3>
         <div>
